9a5e6c4cfa64683ff6f7c32d868f2b5279d7179e
Thanathip <dannajaeiei@hotmail.com>
2022-10-23 18:37:51 +0700

    fix: arrow icon on homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Video } from "../../components";
+import { FaChevronDown } from "react-icons/fa";
 
 const Home: React.FC = () => {
 	return (
@@ -9,8 +10,8 @@ const Home: React.FC = () => {
 				<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80">
 					<img src="../src/assets/images/center-logo.png" />
 				</div>
-				<a href="#" className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-2xl">
-					<i className="fa-solid fa-chevron-down fa-2xl"></i>
+				<a href="#" className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+					<FaChevronDown className="text-3xl" />
 				</a>
 			</Video>
 		</div>
