7350aeb625af00cb407c9a965c46baede9a582bf
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2022-10-23 13:17:38 +0700

    add video overlay

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import bangkokVideo from "../assets/videos/bangkok-video1.mp4";
 import "./Video.css";
 const Video = () => {
-  return <video src={bangkokVideo} autoPlay loop muted></video>;
+  return (
+    <div className="overlay">
+      <video src={bangkokVideo} autoPlay loop muted></video>
+    </div>
+  );
 };
 
 export default Video;
