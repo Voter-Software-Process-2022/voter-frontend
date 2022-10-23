52c9c240c6f6f7edc0da4d39cb9441eb7345be31
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2022-10-23 21:54:32 +0700

    import font

--- src/components/VoteSection/index.tsx
+++ src/components/VoteSection/index.tsx
@@ -3,8 +3,8 @@ import "./VoteSection.css";
 const VoteSection = () => {
   return (
     <div id="vote" className="vote-container">
-      <div className="vote-container-left">Vote Container Left</div>
-      <div className="vote-container-right">Vote Container Right</div>
+      <div className="vote-container-left">Vote For Mp</div>
+      <div className="vote-container-right">Vote For Party</div>
     </div>
   );
 };
