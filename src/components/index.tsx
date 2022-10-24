a87191a0cd749733b21809de7530daaa4ae05510
Thanathip <dannajaeiei@hotmail.com>
2022-10-24 18:36:06 +0700

    refactor: reformat code using prettier

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,2 @@
-export { default as Navbar } from "./Navbar";
-export { default as Video } from "./Video";
+export { default as Navbar } from './Navbar'
+export { default as Video } from './Video'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,2 @@
-export { default as Home } from "./Home";
-export { default as SignUp } from "./SignUp";
+export { default as Home } from './Home'
+export { default as SignUp } from './SignUp'
