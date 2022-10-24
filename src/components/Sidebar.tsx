8de428d9359dbb217785b8ba81e8680c61b2e947
Thanathip <dannajaeiei@hotmail.com>
2022-10-24 23:38:00 +0700

    refactor: added subtopic of sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,44 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { SidebarProps } from '../interfaces/components/sidebar'
+import SidebarLink from './SidebarLink'
+import { IoPersonSharp } from 'react-icons/io5'
+import useOutsideAlerter from '../hooks/useOutsideAlerter'
+import { MENU_LIST } from '../config/menu'
 
 const Sidebar: React.FC<SidebarProps> = ({
   isOpenSidebar,
   setIsOpenSidebar,
 }) => {
-  function useOutsideAlerter(ref: React.RefObject<HTMLInputElement>) {
-    useEffect(() => {
-      function handleClickOutside(event: MouseEvent) {
-        const target = event.target as HTMLInputElement
-        if (ref.current && !ref.current.contains(target)) {
-          setIsOpenSidebar(false)
-        }
-      }
-      document.addEventListener('mousedown', handleClickOutside)
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside)
-      }
-    }, [ref])
-  }
-
   const wrapperRef = useRef(null)
-  useOutsideAlerter(wrapperRef)
+  useOutsideAlerter(wrapperRef, setIsOpenSidebar)
 
   return (
     <div
-      className={`w-[350px] h-[calc(100vh-88px)] bg-red-500 absolute top-0 duration-500 ${
+      className={`w-[350px] h-[calc(100vh-88px)] bg-navbar absolute top-0 duration-500 ${
         isOpenSidebar ? 'right-0' : '-right-[350px]'
-      }`}
+      } flex flex-col justify-between`}
       ref={wrapperRef}
-    ></div>
+    >
+      <div className='flex flex-col'>
+        {MENU_LIST.map((list, index) => (
+          <SidebarLink
+            topic={list.topic}
+            subTopic={list.subTopic}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className='mb-8'>
+        <div className='py-4 hover:border-r-4 hover:border-[#632ce4] hover:bg-[#ffffff26] cursor-pointer'>
+          <div className='flex items-center mx-8'>
+            <IoPersonSharp className='text-lg mr-4' />
+            <span className='text-slate-300 text-xl font-medium'>
+              Sign In / Sign Up
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
