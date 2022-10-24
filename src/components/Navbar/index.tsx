0f4083595257a4604de78e7dd57b3cb826dbd729
Thanathip <dannajaeiei@hotmail.com>
2022-10-24 20:11:28 +0700

    feat: added bar on responsive design

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { IoPersonSharp } from 'react-icons/io5'
+import { HiOutlineBars3BottomRight, HiOutlineXMark } from 'react-icons/hi2'
+import { NavbarProps } from '../../interfaces/components/navbar'
 
 const NAVBAR_LIST = ['Home', 'Vote', 'Information', 'About']
 
-const Navbar: React.FC = () => {
+const Navbar: React.FC<NavbarProps> = ({ isOpenSidebar, setIsOpenSidebar }) => {
   return (
     <div className='w-full flex items-center justify-between py-3 px-12 bg-navbar'>
       <div className='flex'>
@@ -15,27 +17,43 @@ const Navbar: React.FC = () => {
             <span>Royal Thai Government</span>
           </div>
         </div>
-        <div className='flex items-center text-lg font-semibold ml-4'>
+        <div className='hidden items-center text-lg font-semibold ml-4 lg:flex'>
           {NAVBAR_LIST.map((topic, index) => (
-            <span className='mx-1 px-2 py-1 cursor-pointer duration-300 hover:bg-[#ffffff26] hover:rounded-lg' key={index}>
+            <span
+              className='mx-1 px-2 py-1 cursor-pointer duration-300 hover:bg-[#ffffff26] hover:rounded-lg'
+              key={index}
+            >
               {topic}
             </span>
           ))}
         </div>
       </div>
-      <div className='flex items-center h-[40px] bg-[#ffffff17] rounded-lg'>
-        <Link to='/register'>
-          <div className='flex items-center px-4 py-2 cursor-pointer duration-300 hover:bg-[#ffffff26] hover:rounded-l-lg '>
-            <IoPersonSharp className='mr-2' />
-            <span>Sign up</span>
-          </div>
-        </Link>
-        <div className='w-[0.5px] bg-white h-[70%]'></div>
-        <Link to='/login'>
-          <div className='px-4 py-2 cursor-pointer duration-300 hover:bg-[#ffffff26] hover:rounded-r-lg'>
-            <span>Sign in</span>
-          </div>
-        </Link>
+      <div className='flex items-center'>
+        {!isOpenSidebar ? (
+          <HiOutlineBars3BottomRight
+            className='text-3xl lg:hidden cursor-pointer'
+            onClick={() => setIsOpenSidebar(true)}
+          />
+        ) : (
+          <HiOutlineXMark
+            className='text-3xl lg:hidden cursor-pointer'
+            onClick={() => setIsOpenSidebar(false)}
+          />
+        )}
+        <div className='hidden lg:flex items-center h-[40px] bg-[#ffffff17] rounded-lg'>
+          <Link to='/register'>
+            <div className='flex items-center px-4 py-2 cursor-pointer duration-300 hover:bg-[#ffffff26] hover:rounded-l-lg '>
+              <IoPersonSharp className='mr-2' />
+              <span>Sign up</span>
+            </div>
+          </Link>
+          <div className='w-[0.5px] bg-white h-[70%]'></div>
+          <Link to='/login'>
+            <div className='px-4 py-2 cursor-pointer duration-300 hover:bg-[#ffffff26] hover:rounded-r-lg'>
+              <span>Sign in</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   )
