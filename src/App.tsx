7899102f909efe1ab2ed7813dc3d2a3cb7a722d8
Thanathip <dannajaeiei@hotmail.com>
2022-10-26 15:21:01 +0700

    fix: added loader component to suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Loader } from './components'
 import Router from './router'
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Router />
       </Suspense>
     </BrowserRouter>
