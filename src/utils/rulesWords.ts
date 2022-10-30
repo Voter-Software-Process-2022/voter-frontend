5f97fd122f257ab9bb8858b0a67ebe3acf89bd8f
bhokin <zbeamz@hotmail.com>
2022-10-30 22:58:08 +0700

    feat: Add word to display on Rules modal

---
+++ src/utils/rulesWords.ts
@@ -0,0 +1,17 @@
+export const thai = {
+  penaltyAndPunishment: {
+    p1: 'Do not take/capture pictures of ballot that can see the voting marks in the polling, if Disobedience, you will get imprisonment for not more than 1 year or a fine of not more than 20,000 baht, or both according to “Section 135”',
+    p2: 'Do not show/share the pictures that have been already voted, if Disobedience, you will get imprisonment for not more than 1 year or a fine of not more than 20,000 baht, or both according to “Section 135”',
+    p3: 'Do not allow third-party that didn’t have right to vote represent/participate in your voting with both intend or not intend, if Disobedience, you will get imprisonment between 1 - 10 years or a fine between 20,000 - 200,000 baht, or both according to “Section 126 Paragraph 1 ” and the court ordered the revocation of the right to vote for 20 years.',
+    p4: 'Do not impersonate or falsify other people\'s id cards, both occupied and not occupied, to be used to vote instead. If Disobedience, it would be illegal under "Section 91 Paragraph two", with penalties in "Section 136", you will get imprisonment for not more than 10 years or a fine of not more than 200,000 baht or both and the court ordered the revocation of the right to vote for 10 years.',
+  },
+}
+
+export const english = {
+  penaltyAndPunishment: {
+    p1: 'ห้ามถ่ายภาพบัตรเลือกตั้ง เพื่อให้เห็นเครื่องหมายลงคะแนนในคูหาเลือกตั้ง หากฝ่าฝืนจะผิดกฎหมายตาม "มาตรา 135" ระวางโทษจำคุกไม่เกิน 1 ปี หรือ ปรับไม่เกิน 20,000 บาท หรือทั้งจำทั้งปรับ',
+    p2: 'อย่านำบัตรเลือกตั้งที่ออกเสียงลงคะแนนแล้ว แสดงต่อผู้อื่น หากฝ่าฝืนจะผิดกฎหมายตาม "มาตรา 135" ระวางโทษจำคุกไม่เกิน 1 ปี หรือ ปรับไม่เกิน 20,000 บาท หรือทั้งจำทั้งปรับ',
+    p3: 'ห้ามให้บุคคลภายนอกที่ไม่มีสิทธิเลือกตั้งมาดำเนินการ การเลือกตั้งแทนคนที่มีสิทธิเลือกตั้ง หากฝ่าฝืนจะผิดกฏหมายตาม “มาตรา 126 วรรคหนึ่ง” ระวางโทษจำคุกตั้งแต่ 1 - 10 ปี หรือ ปรับตั้งแต่ 20,000 - 200,000 บาท หรือทั้งจำทั้งปรับ และศาลสั่งเพิกถอนสิทธิการเลือกตั้งเป็นเวลา 20 ปี',
+    p4: 'ห้ามแอบอ้างหรือปลอมแปลง บัตรประจำตัวประชาชนของคนอื่น ทั้งครอบครองและไม่ได้ครอบครอง เพื่อมาใช้สิทธิเลือกตั้งแทน หากฝ่าฝืนจะผิดกฏหมายตาม “มาตรา 91 วรรคสอง” โดยจะใช้บทลงโทษใน “มาตรา 136” ระวางโทษจำคุกไม่เกิน 10 ปี หรืป ปรับไม่เกิน 200,000 บาท หรืทั้งจำทั้งปรับ และศาลสั่งเพิกถอนสิทธิการเลือกตั้งเป็นเวลา 10 ปี',
+  },
+}
