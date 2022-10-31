5e0b3fd93c4c55dfe640b70210462d6b7cee3c90
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2022-10-31 13:02:33 +0700

    add button for vote section

--- src/components/VoteSection.tsx
+++ src/components/VoteSection.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 const VoteSection: React.FC = () => {
   return (
-    <div className='h-[25vh] lg:h-[50vh] flex justify-around text-black '>
-      <div className='w-[100%] bg-white items-center'>
-        <p className='text-center text-[1rem] mt-2 md:text-[1.5rem] lg:text-[2rem] md:mt-10 font-bold'>
-          Vote For MP
-        </p>
-      </div>
-      <div className='w-[100%] bg-stone-300'>
-        <p className='text-center text-[1rem] mt-2 md:text-[1.5rem] lg:text-[2rem] md:mt-10 font-bold'>
+    <div className='h-[25vh] lg:h-[50vh] flex justify-around text-black  '>
+      <div className='w-[100%] bg-white text-center'>
+        <p className='text-[1rem] mt-2 md:text-[1.5rem] lg:text-[2rem] md:mt-5 font-bold'>
           Vote For Party
         </p>
+        <button className=' p-2.5 rounded-md mt-[70px] md:mt-[50px] md:p-1 xl:mt-[16rem] xl:p-[1.5rem] ease-in-out delay-150 bg-gray-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ...'>
+          See Details
+        </button>
+      </div>
+      <div className='w-[100%] bg-stone-300 text-center'>
+        <p className='text-[1rem] mt-2 md:text-[1.5rem] lg:text-[2rem] md:mt-5 font-bold'>
+          Vote For Party
+        </p>
+        <button className=' p-2.5 rounded-md mt-[70px] md:mt-[50px] md:p-1 xl:mt-[16rem] xl:p-[1.5rem] ease-in-out delay-150 bg-gray-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ...'>
+          See Details
+        </button>
       </div>
     </div>
   )
