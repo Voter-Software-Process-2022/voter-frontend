f210c52fa48929795375aba7c9004dade3d96472
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2022-11-05 17:40:18 +0700

    add middle component

--- src/components/MiddleInfo.tsx
+++ src/components/MiddleInfo.tsx
@@ -1,11 +1,122 @@
 import React from 'react'
+import CandidateCard from './CandidateCard'
 
 const MiddleInfo = () => {
   return (
     <div
       id='vote-section'
-      className='h-[60vh] lg:h-[80vh] flex justify-around text-black   bg-gradient-to-br from-sky-300 to-blue-800'
-    ></div>
+      className='h-[60vh] lg:h-[80vh] text-black flex justify-around bg-[#3850ba] p-[4rem]'
+    >
+      <div className='p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+        <svg
+          className='mb-2 w-10 h-10 text-gray-500 dark:text-gray-400'
+          aria-hidden='true'
+          fill='currentColor'
+          viewBox='0 0 20 20'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fill-rule='evenodd'
+            d='M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z'
+            clip-rule='evenodd'
+          ></path>
+          <path d='M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z'></path>
+        </svg>
+        <a href='#'>
+          <h5 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+            Need a help in Claim?
+          </h5>
+        </a>
+        <p className='mb-3 font-normal text-gray-500 dark:text-gray-400'>
+          Go to this step by step guideline process on how to certify for your
+          weekly benefits:
+        </p>
+        <a
+          href='#'
+          className='inline-flex items-center text-blue-600 hover:underline'
+        >
+          See our guideline
+          <svg
+            className='ml-2 w-5 h-5'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z'></path>
+            <path d='M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z'></path>
+          </svg>
+        </a>
+      </div>
+      <div className='p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+        <a href='#'>
+          <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </a>
+        <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+        <a
+          href='#'
+          className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        >
+          Read more
+          <svg
+            aria-hidden='true'
+            className='ml-2 -mr-1 w-4 h-4'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fill-rule='evenodd'
+              d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+              clip-rule='evenodd'
+            ></path>
+          </svg>
+        </a>
+      </div>
+      <div className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+        <a href='#'>
+          <img
+            className='rounded-t-lg'
+            src='https://flowbite.com/docs/images/blog/image-1.jpg'
+            alt=''
+          />
+        </a>
+        <div className='p-5'>
+          <a href='#'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href='#'
+            className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+          >
+            Read more
+            <svg
+              aria-hidden='true'
+              className='ml-2 -mr-1 w-4 h-4'
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fill-rule='evenodd'
+                d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
+                clip-rule='evenodd'
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
