cebc924b44d0ab271c8aae89d2fc13b69ca3d23c
wave49192 <sirawich.tu@ku.th>
2022-11-05 23:26:20 +0700

    remove text

--- src/components/UpperInfo.tsx
+++ src/components/UpperInfo.tsx
@@ -4,7 +4,7 @@ const UpperInfo: React.FC = () => {
   return (
     <div>
       <div className='h-[50vh] lg:h-[75vh]  flex justify-around text-black  '>
-        <div className='w-[100%] bg-white text-center xl:pt-[2rem]'>
+        <div className='w-[100%] bg-white text-center xl:pt-[1rem]'>
           <h1 className='text-[1.25rem] pt-4 md:text-[1.5rem] lg:text-[2rem] md:mt-5 font-bold'>
             Detail 1
           </h1>
@@ -17,14 +17,13 @@ const UpperInfo: React.FC = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit.
+            aliquip ex ea commodo consequat.
           </p>
           {/* <button className=' p-2.5 rounded-md mt-[70px] md:mt-[60px] md:p-2  xl:p-[1.5rem] inline-block px-6 py-2.5 bg-[#19303d] text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-ou'>
             See Details
           </button> */}
         </div>
-        <div className='w-[100%] bg-stone-300 text-center xl:pt-[2rem]'>
+        <div className='w-[100%] bg-stone-300 text-center xl:pt-[1rem]'>
           <h1 className='text-[1.25rem] pt-4 md:text-[1.5rem] lg:text-[2rem] md:mt-5 font-bold'>
             Detail 2
           </h1>
@@ -37,8 +36,7 @@ const UpperInfo: React.FC = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit.
+            aliquip ex ea commodo consequat.
           </p>
           {/* <button className=' p-2.5 rounded-md mt-[70px] md:mt-[60px] md:p-2  xl:p-[1.5rem] inline-block px-6 py-2.5 bg-[#19303d] text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-ou'>
             See Details
