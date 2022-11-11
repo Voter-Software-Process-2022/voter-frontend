3a324ac83c2f5278270e963500e68e28624b06ee
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2022-11-11 16:31:37 +0700

    comment see details button

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -20,14 +20,14 @@ const InfoCard: React.FC<InfoList> = ({ id, name, description }) => {
           <span className='text-lg text-gray-500 dark:text-gray-400'>
             {description}
           </span>
-          <div className='flex mt-4 space-x-3 md:mt-6'>
+          {/* <div className='flex mt-4 space-x-3 md:mt-6'>
             <a
               href='#'
               className='inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
             >
               See Details
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
