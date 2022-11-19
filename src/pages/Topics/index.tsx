942adf9e3303e9d1f742a29045171310b8c58817
bhokin <zbeamz@hotmail.com>
2022-11-19 18:58:11 +0700

    feat(bug): apply search to topics pages (bug occur with filter function

--- src/pages/Topics/index.tsx
+++ src/pages/Topics/index.tsx
@@ -1,9 +1,26 @@
 import React, { useState } from 'react'
 import { Navbar, Sidebar, TopicCard } from '../../components'
+import SearchBar from '../../components/SearchBar'
 import { TOPIC_LIST } from '../../config/mockTopics'
 
 const Topics = () => {
   const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(false)
+  const [filterWord, setFilterWord] = useState<string>('')
+
+  const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterWord(e.target.value)
+    console.log(
+      TOPIC_LIST[0].topicName
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase()),
+    )
+  }
+
+  const filteredTopicList = TOPIC_LIST.filter((topic) => {
+    topic.topicName.toLowerCase().includes(filterWord.toLowerCase())
+  })
+
+  const selectedTopicList = filterWord === '' ? TOPIC_LIST : filteredTopicList
 
   return (
     <div className='w-full overflow-x-hidden bg-white min-h-screen'>
@@ -17,9 +34,10 @@ const Topics = () => {
           setIsOpenSidebar={setIsOpenSidebar}
         />
       </div>
-      <div className='flex justify-center mx-auto max-w-4xl py-16 px-8 sm:py-24 sm:px-6 lg:min-w-[56rem] lg:max-w-7xl lg:px-8'>
+      <div className='flex flex-col justify-center mx-auto max-w-4xl py-16 px-8 sm:py-24 sm:px-6 lg:min-w-[56rem] lg:max-w-7xl lg:px-8'>
+        <SearchBar onChangeHandler={searchHandler} />
         <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-          {TOPIC_LIST.map((topic) => (
+          {selectedTopicList.map((topic) => (
             <TopicCard
               key={topic.topicId}
               topicId={topic.topicId}
