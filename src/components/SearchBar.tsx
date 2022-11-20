1ba9445ef06b493d0231515bb51f843b69f71c87
bhokin <zbeamz@hotmail.com>
2022-11-20 18:45:41 +0700

    feat: adjust size search bar & reponsive

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,14 +11,14 @@ const SearchBar: React.FC<SearchBarProps> = ({
   onChangeHandler,
 }) => {
   return (
-    <div className='pt-2 relative mx-auto text-gray-600'>
-      <div className='flex flex-row border-2 px-5 border-gray-300 bg-white rounded-full'>
+    <div className='pt-2 relative mx-auto xs:w-full md:w-auto text-gray-600'>
+      <div className='flex flex-row border-2 px-5 mr-2 border-gray-300 bg-white rounded-full'>
         <div className='mr-3 self-center'>
           <IoSearch />
         </div>
         <div>
           <input
-            className='bg-white h-10 w-56 rounded-lg text-sm focus:outline-none'
+            className='bg-white h-10 rounded-lg text-sm focus:outline-none sm:w-96'
             type='search'
             name='search'
             placeholder={placeholderWord}
