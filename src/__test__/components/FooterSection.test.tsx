15c32135b6c127bf0f8f02812e01e8f1729375da
ParnThanatibordee <parn8946@gmail.com>
2022-11-21 14:34:04 +0700

    add test in FooterSection.test.tsx

--- src/__test__/components/FooterSection.test.tsx
+++ src/__test__/components/FooterSection.test.tsx
@@ -48,7 +48,126 @@ describe('FooterSection', () => {
     const developerLink = screen.getByText('Yak3BdPro')
     expect(developerLink).toBeDefined()
 
-    fireEvent.click(governmentLogo)
-    // expect(history.location.pathname).toContain('/github.com/Voter-Software-Process-2022')
+    expect(governmentLogo.getAttribute("href")).toBe("https://github.com/Voter-Software-Process-2022")
+  })
+
+  test('click about link', () => {
+    render(
+      <BrowserRouter>
+        <FooterSection />
+      </BrowserRouter>,
+    )
+
+    const governmentLogo = screen.getByTestId('royal-thai-government')
+    expect(governmentLogo).toBeDefined()
+    expect(governmentLogo.textContent).toBe('Royal Thai Government')
+    const aboutLink = screen.getByText('About')
+    expect(aboutLink).toBeDefined()
+    const privacyLink = screen.getByText('Privacy Policy')
+    expect(privacyLink).toBeDefined()
+    const gitHubLink = screen.getByText('Github')
+    expect(gitHubLink).toBeDefined()
+    const contactLink = screen.getByText('Contact Us')
+    expect(contactLink).toBeDefined()
+    const developerLink = screen.getByText('Yak3BdPro')
+    expect(developerLink).toBeDefined()
+
+    expect(aboutLink.getAttribute("href")).toBe("#")
+  })
+
+  test('click privacy link', () => {
+    render(
+      <BrowserRouter>
+        <FooterSection />
+      </BrowserRouter>,
+    )
+
+    const governmentLogo = screen.getByTestId('royal-thai-government')
+    expect(governmentLogo).toBeDefined()
+    expect(governmentLogo.textContent).toBe('Royal Thai Government')
+    const aboutLink = screen.getByText('About')
+    expect(aboutLink).toBeDefined()
+    const privacyLink = screen.getByText('Privacy Policy')
+    expect(privacyLink).toBeDefined()
+    const gitHubLink = screen.getByText('Github')
+    expect(gitHubLink).toBeDefined()
+    const contactLink = screen.getByText('Contact Us')
+    expect(contactLink).toBeDefined()
+    const developerLink = screen.getByText('Yak3BdPro')
+    expect(developerLink).toBeDefined()
+
+    expect(privacyLink.getAttribute("href")).toBe("#")
+  })
+
+  test('click github link', () => {
+    render(
+      <BrowserRouter>
+        <FooterSection />
+      </BrowserRouter>,
+    )
+
+    const governmentLogo = screen.getByTestId('royal-thai-government')
+    expect(governmentLogo).toBeDefined()
+    expect(governmentLogo.textContent).toBe('Royal Thai Government')
+    const aboutLink = screen.getByText('About')
+    expect(aboutLink).toBeDefined()
+    const privacyLink = screen.getByText('Privacy Policy')
+    expect(privacyLink).toBeDefined()
+    const gitHubLink = screen.getByText('Github')
+    expect(gitHubLink).toBeDefined()
+    const contactLink = screen.getByText('Contact Us')
+    expect(contactLink).toBeDefined()
+    const developerLink = screen.getByText('Yak3BdPro')
+    expect(developerLink).toBeDefined()
+
+    expect(gitHubLink.getAttribute("href")).toBe("#")
+  })
+
+  test('click contract link', () => {
+    render(
+      <BrowserRouter>
+        <FooterSection />
+      </BrowserRouter>,
+    )
+
+    const governmentLogo = screen.getByTestId('royal-thai-government')
+    expect(governmentLogo).toBeDefined()
+    expect(governmentLogo.textContent).toBe('Royal Thai Government')
+    const aboutLink = screen.getByText('About')
+    expect(aboutLink).toBeDefined()
+    const privacyLink = screen.getByText('Privacy Policy')
+    expect(privacyLink).toBeDefined()
+    const gitHubLink = screen.getByText('Github')
+    expect(gitHubLink).toBeDefined()
+    const contactLink = screen.getByText('Contact Us')
+    expect(contactLink).toBeDefined()
+    const developerLink = screen.getByText('Yak3BdPro')
+    expect(developerLink).toBeDefined()
+
+    expect(contactLink.getAttribute("href")).toBe("#")
+  })
+
+  test('click developer link', () => {
+    render(
+      <BrowserRouter>
+        <FooterSection />
+      </BrowserRouter>,
+    )
+
+    const governmentLogo = screen.getByTestId('royal-thai-government')
+    expect(governmentLogo).toBeDefined()
+    expect(governmentLogo.textContent).toBe('Royal Thai Government')
+    const aboutLink = screen.getByText('About')
+    expect(aboutLink).toBeDefined()
+    const privacyLink = screen.getByText('Privacy Policy')
+    expect(privacyLink).toBeDefined()
+    const gitHubLink = screen.getByText('Github')
+    expect(gitHubLink).toBeDefined()
+    const contactLink = screen.getByText('Contact Us')
+    expect(contactLink).toBeDefined()
+    const developerLink = screen.getByText('Yak3BdPro')
+    expect(developerLink).toBeDefined()
+
+    expect(developerLink.getAttribute("href")).toBe("https://github.com/Voter-Software-Process-2022")
   })
 })
