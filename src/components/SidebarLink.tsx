9c7208747ae9c365e60379279ba3034c0dae579f
ParnThanatibordee <parn8946@gmail.com>
2022-11-22 01:22:03 +0700

    add SidebarLink.test.tsx

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -27,7 +27,7 @@ const SidebarLink: React.FC<MenuList> = ({
         className='py-4 border-r-4 border-navbar hover:border-r-4 hover:border-[#632ce4] hover:bg-[#ffffff26] cursor-pointer'
       >
         <div className='flex items-center justify-between mx-8'>
-          <span className='text-slate-300 text-xl font-medium'>{topic}</span>
+          <span className='text-slate-300 text-xl font-medium' data-testid='sidebar-topic'>{topic}</span>
           {hasSubTopic && (
             <AiOutlineCaretDown
               className={`duration-300 ${subnav ? '-rotate-180' : ''}`}
