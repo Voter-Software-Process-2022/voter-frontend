8be5ba82ad7ebce22091ff241a8d3fb9aa085884
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-24 20:38:00 +0700

    feat: fetch data and map candidate info

--- src/components/InfoList.tsx
+++ src/components/InfoList.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import type { InfoListProps } from '../interfaces/components/infoList'
 import InfoCard from './InfoCard'
 
-const InfoList: React.FC = () => {
+const InfoList: React.FC<InfoListProps> = ({ voteTopicId, candidates }) => {
   return (
     <div className='bg-white p-8'>
       <div className='mt-10 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 xl:grid-cols-4 xl:gap-x-8 text-black'>
-        <InfoCard id={1} name={'John Ocha'} description='description here' />
-        <InfoCard id={2} name={'john2'} description='description here' />
-        <InfoCard id={3} name={'john3'} description='description here' />
-        <InfoCard id={4} name={'john4'} description='description here' />
-        <InfoCard id={5} name={'john5'} description='description here' />
-        <InfoCard id={6} name={'john6'} description='description here' />
-        <InfoCard id={7} name={'john7'} description='description here' />
-        <InfoCard id={8} name={'john8'} description='description here' />
+        {candidates.map((candidate) => (
+          <InfoCard
+            voteTopicId={voteTopicId}
+            id={candidate.id}
+            name={candidate.name}
+            pictureUrl={candidate.pictureUrl}
+            key={candidate.id}
+          />
+        ))}
       </div>
     </div>
   )
