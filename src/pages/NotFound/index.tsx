1f172f1696c1b60c6e88e64b493422a4c5c38f86
tboonma <tboonma@hotmail.com>
2022-11-24 21:38:03 +0700

    Revert "add forbidden pages"
    This reverts commit db8015fde2bf1c6ecca31398fa59b8448a82571d.

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
@@ -11,12 +10,12 @@ const NotFound = () => {
       <div className='inset-0 bg-black opacity-25 absolute'></div>
       <div className='container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40'>
         <div className='w-full font-mono flex flex-col items-center relative z-10'>
-          <h1 className='font-extrabold text-5xl text-center text-white leading-tight pt-[5rem] animate-bounce '>
-            404 not Found
+          <h1 className='font-extrabold text-5xl text-center text-white leading-tight pt-[5rem] '>
+            You are all alone here
           </h1>
-          <Link to='/' className='my-44'>
-            <p className='font-extrabold text-8xl  text-white '>Go Back</p>
-          </Link>
+          <p className='font-extrabold text-8xl my-44 text-white animate-bounce'>
+            404
+          </p>
         </div>
       </div>
     </div>
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -9,7 +9,6 @@ const ThankYouPage = lazy(() => import('../pages/ThankYouPage'))
 const Topics = lazy(() => import('../pages/Topics'))
 const NotFound = lazy(() => import('../pages/NotFound'))
 const Vote = lazy(() => import('../pages/Vote'))
-const Forbidden = lazy(() => import('../pages/Forbidden'))
 const routes = (user: IUser) => [
   {
     path: '/',
@@ -26,7 +25,6 @@ const routes = (user: IUser) => [
   { path: '/thank-you', element: <ThankYouPage /> },
   { path: '/topics', element: <Topics /> },
   { path: '*', element: <NotFound /> },
-  { path: '/403', element: <Forbidden /> },
 ]
 
 export default routes
