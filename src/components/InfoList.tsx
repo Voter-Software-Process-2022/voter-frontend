54d84bc1446139f2ec2ae5c652098f68c3ac4bcb
Thanathip <dannajaeiei@hotmail.com>
2022-11-24 16:24:29 +0700

    fix: infocard style and add sidebar on info page

--- src/components/InfoList.tsx
+++ src/components/InfoList.tsx
@@ -4,7 +4,7 @@ import InfoCard from './InfoCard'
 const InfoList: React.FC = () => {
   return (
     <div className='bg-white p-8'>
-      <div className='mt-10 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 text-black px-32'>
+      <div className='mt-10 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 xl:grid-cols-4 xl:gap-x-8 text-black'>
         <InfoCard id={1} name={'John Ocha'} description='description here' />
         <InfoCard id={2} name={'john2'} description='description here' />
         <InfoCard id={3} name={'john3'} description='description here' />
