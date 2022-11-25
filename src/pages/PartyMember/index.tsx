3ae43cfbbbb269aee7b6d373f1bec92b2e2d282b
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-26 02:27:07 +0700

    fix: Typo and loader component

--- src/pages/PartyMember/index.tsx
+++ src/pages/PartyMember/index.tsx
@@ -27,8 +27,6 @@ const CandidateDetail: React.FC = () => {
     fetchPartyMembersData()
   }, [partyId])
 
-  if (isLoading) return <Loader />
-
   return (
     <div className='w-full overflow-x-hidden bg-white min-h-screen'>
       <Navbar
@@ -40,30 +38,34 @@ const CandidateDetail: React.FC = () => {
           isOpenSidebar={isOpenSidebar}
           setIsOpenSidebar={setIsOpenSidebar}
         />
-        <div className='max-w-6xl mx-auto h-32 text-slate-900 p-8'>
-          <span className='text-4xl'>Party: {partyId}</span>
-          <div className='mt-6'>
-            <span className='text-2xl'>Members</span>
-            <div className='flex flex-col mt-4'>
-              {candidates &&
-                candidates.map((candidate) => (
-                  <div
-                    className='flex items-center border text-white border-zinc-800 my-2 bg-gray-800 p-2 rounded'
-                    key={candidate.id}
-                  >
-                    <div className='w-44 h-44 mr-12'>
-                      <img
-                        src={candidate.pictureUrl}
-                        alt={candidate.name}
-                        className='h-full w-full object-cover rounded-full'
-                      />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className='max-w-6xl mx-auto h-32 text-slate-900 p-8'>
+            <span className='text-4xl'>Party: {partyId}</span>
+            <div className='mt-6'>
+              <span className='text-2xl'>Members</span>
+              <div className='flex flex-col mt-4'>
+                {candidates &&
+                  candidates.map((candidate) => (
+                    <div
+                      className='flex items-center border text-white border-zinc-800 my-2 bg-gray-800 p-2 rounded'
+                      key={candidate.id}
+                    >
+                      <div className='w-44 h-44 mr-12'>
+                        <img
+                          src={candidate.pictureUrl}
+                          alt={candidate.name}
+                          className='h-full w-full object-cover rounded-full'
+                        />
+                      </div>
+                      <span className='text-xl'>Name: {candidate.name}</span>
                     </div>
-                    <span className='text-xl'>ชื่อ: {candidate.name}</span>
-                  </div>
-                ))}
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
